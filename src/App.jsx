823c18d782a1af5c3632264588dfdf4e6b97543f
Titly Dey <titlydey60@gmail.com>
2025-03-31 11:53:01 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import HeroSection from "d:/ReactPrj1/project-1/src/components/Hero.jsx";
+import Navigation from "d:/ReactPrj1/project-1/src/components/Navigation";
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "d:/ReactPrj1/project-1/src/App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
